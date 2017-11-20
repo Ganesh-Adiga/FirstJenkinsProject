8a11e57bf4593072434514ca1e74094b78116ac0
ganeshadiga.r <ganeshadiga.r@LP1-AP-51708981.PROD.HCLPNP.COM>
2017-11-20 15:05:43 +0530

    Hello world git auto push

--- src/com/hcl/first/jenkins/project/HelloWorld.java
+++ src/com/hcl/first/jenkins/project/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	
 	public void sayHello() {
 		
-		System.out.println("Hello World Modified and pushed to git. Again Modified");
+		System.out.println("Hello World Git Auto Push");
 
 	}
 
