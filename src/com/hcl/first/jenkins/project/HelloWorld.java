1d1adc2419b9ab90ed08e868a8dc170d3d7cc64d
ganeshadiga.r <ganeshadiga.r@LP1-AP-51708981.PROD.HCLPNP.COM>
2017-11-20 11:21:12 +0530

    Sample Project for Configuring GIT with Jenkins

---
+++ src/com/hcl/first/jenkins/project/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hcl.first.jenkins.project;
+
+public class HelloWorld {
+	
+	public void sayHello() {
+		
+		System.out.println("Hello World !!!!!!!!!!");
+
+	}
+
+}
---
+++ src/com/hcl/first/jenkins/project/HelloWorldTester.java
@@ -0,0 +1,10 @@
+package com.hcl.first.jenkins.project;
+
+public class HelloWorldTester {
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.sayHello();
+	}
+
+}
